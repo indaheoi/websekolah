e2dc4b40a961d05473eaf8ed579c38efef9beafd
indaheoi <160800583+indaheoi@users.noreply.github.com>
2024-12-02 10:37:57 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <footer class="bg-base-200 py-6">
+  <div class="container mx-auto text-center">
+    <p class="text-sm">© 2024 Sekolah XYZ. All rights reserved.</p>
+    <div class="flex justify-center space-x-4 mt-2">
+      <a href="#" class="link link-hover">Kebijakan Privasi</a>
+      <a href="#" class="link link-hover">Syarat dan Ketentuan</a>
+    </div>
+  </div>
+</footer>
 
   </div>
 </template>
